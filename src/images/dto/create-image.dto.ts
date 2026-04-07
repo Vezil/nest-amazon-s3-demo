817724b535948d922661b9a41679127e91cecd49
Szymon Wojaczek <szymon.wojaczek@linkedclient.com>
2026-04-07 10:58:26 +0200

    FIX: validation

--- src/images/dto/create-image.dto.ts
+++ src/images/dto/create-image.dto.ts
@@ -1,13 +1,16 @@
+import { Type } from 'class-transformer';
 import { IsInt, IsString, Min } from 'class-validator';
 
 export class CreateImageDto {
   @IsString()
   title: string;
 
+  @Type(() => Number)
   @IsInt()
   @Min(1)
   width: number;
 
+  @Type(() => Number)
   @IsInt()
   @Min(1)
   height: number;
--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -10,12 +10,38 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { memoryStorage } from 'multer';
 import { ImagesService } from './images.service';
 import { CreateImageDto } from './dto/create-image.dto';
+import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Images')
 @Controller('images')
 export class ImagesController {
   constructor(private readonly imagesService: ImagesService) {}
 
   @Post()
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+        title: {
+          type: 'string',
+        },
+        width: {
+          type: 'integer',
+          example: 800,
+        },
+        height: {
+          type: 'integer',
+          example: 600,
+        },
+      },
+      required: ['file', 'title', 'width', 'height'],
+    },
+  })
   @UseInterceptors(
     FileInterceptor('file', {
       storage: memoryStorage(),
